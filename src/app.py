5db61db7d258d8422f1b7d642e9c010cde3d86cc
Dario Contreras <dario.contreras@ibhsolutions.com>
2021-04-20 10:05:36 -0400

    Adding new file

--- src/app.py
+++ src/app.py
@@ -14,8 +14,8 @@ def create_app(*, settings: Settings = None) -> core.App:
     env = get_env(settings=settings)
     app = core.App()
     # for stage in STAGES:
-    stage = "dev"
-    s3.S3Stack(app, settings=settings, stage=str(stage), env=env)
-    glue.GlueStack(app, settings=settings, stage=str(stage), env=env)
+    for stage in STAGES:
+        s3.S3Stack(app, settings=settings, stage=str(stage), env=env)
+        glue.GlueStack(app, settings=settings, stage=str(stage), env=env)
 
     return app
